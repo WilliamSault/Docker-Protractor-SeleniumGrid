5ff781f9028e11669b9518f971e9493dd51e5b3b
William Sault <williamrsault@gmail.com>
2018-08-22 16:30:36 +0100

    slimmed image size using alpine version of node and jre8

--- RunTests.sh
+++ RunTests.sh
@@ -2,4 +2,4 @@
 # run the tests
 docker container run --rm --network="host" -v "$(PWD)/Protractor/Tests":/usr/src/app/Tests williamsault/protractor:latest
 # Create the report and host results on port 8800
-docker container run --rm -d --name allure -p 8800:8800 -v  "${PWD}/Protractor/Tests/allure-results":/allure-results williamsault/allure
+docker container run --rm --name allure -p 8800:8800 -v  "${PWD}/Protractor/Tests/allure-results":/allure-results williamsault/allure
